eb5c73191e973c44f514401d1892ab721a7b5379
Jim Ryan <jim@room118solutions.com>
2014-04-22 17:17:55 -0400

    Add attribute for SenderHeaders value

--- postfix-dkim/attributes/default.rb
+++ postfix-dkim/attributes/default.rb
@@ -3,6 +3,7 @@ default['postfix_dkim']['domain'] = node[:fqdn]
 default['postfix_dkim']['keyfile'] = '/etc/mail/dkim.key'
 default['postfix_dkim']['selector'] = 'mail'
 default['postfix_dkim']['autorestart'] = false
+default['postfix_dkim']['sender_headers'] = nil
 
 # /etc/default/opendkim
 default['postfix_dkim']['socket'] = 'inet:8891@localhost' # Ubuntu default - listen on loopback on port 8891
