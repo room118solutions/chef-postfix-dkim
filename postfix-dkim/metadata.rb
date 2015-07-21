6b0f1abf2ecc82d900e0ba7ac6ce13f7eed991a7
Jim Ryan <jim@room118solutions.com>
2015-07-21 16:26:10 -0400

    Version bump

--- postfix-dkim/metadata.rb
+++ postfix-dkim/metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "info@room118solutions.com"
 license          "Apache 2.0"
 description      "Installs/Configures postfix and opendkim, a postfix DKIM filter (see: https://help.ubuntu.com/community/Postfix/DKIM)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 depends          'postfix', '> 3.0.0'
