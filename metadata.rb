0119778ef30e7fbe09602954b6ddb6a0e5ce1325
Jim Ryan <jim@room118solutions.com>
2015-07-21 16:51:57 -0400

    Add issues_url and source_url metadata

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,7 @@ maintainer_email "info@room118solutions.com"
 license          "Apache 2.0"
 description      "Installs/Configures postfix and opendkim, a postfix DKIM filter (see: https://help.ubuntu.com/community/Postfix/DKIM)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.1"
+version          "1.0.2"
 depends          'postfix', '> 3.0.0'
+issues_url       "https://github.com/room118solutions/chef-postfix-dkim/issues"
+source_url       "https://github.com/room118solutions/chef-postfix-dkim"
