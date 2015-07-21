cb7d5e4f031607f881a4f44f347abae8bd176d78
Jim Ryan <jim@room118solutions.com>
2015-07-21 16:25:31 -0400

    Update metadata to reflect new markdown readme

--- postfix-dkim/metadata.rb
+++ postfix-dkim/metadata.rb
@@ -3,6 +3,6 @@ maintainer       "Room 118 Solutions, Inc."
 maintainer_email "info@room118solutions.com"
 license          "Apache 2.0"
 description      "Installs/Configures postfix and opendkim, a postfix DKIM filter (see: https://help.ubuntu.com/community/Postfix/DKIM)"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 depends          'postfix', '> 3.0.0'
